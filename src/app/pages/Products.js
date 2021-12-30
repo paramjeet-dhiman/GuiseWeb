7c5772492115624e26580151286ab5b16071e962
paramjeet-dhiman <paramjeet@guise.ai>
2021-12-30 21:07:55 +0530

    .

--- src/app/pages/Products.js
+++ src/app/pages/Products.js
@@ -4,7 +4,6 @@ import airplaneProduct from "../../assets/images/airplane_product.jpg";
 import audioProduct from "../../assets/images/audio_product.jpeg";
 import signageProduct from "../../assets/images/ds_product.jpeg";
 import energyProduct from "../../assets/images/energy_product.jpeg";
-import trafficProduct from "../../assets/images/traffic_product.jpeg";
 
 export const Products = () => {
   const productsInfo = [
@@ -27,9 +26,11 @@ export const Products = () => {
       url: `/products/intelligent-audio-overview`,
     },
     {
+      // https://images.unsplash.com/photo-1504735432588-ab3bf116740f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1075&q=80
+
       name: "Traffic Management",
       description: `Revolutionize vehicle and crowd management with accurate analytics for better planning and design of city routes. Smart Cities rise from smart analytics.`,
-      img: trafficProduct,
+      img: `https://images.unsplash.com/photo-1563980714252-74345152712e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80`,
       url: `/products/traffic-management-overview`,
     },
     {
