04c415c055027fe650887f2fdc8960b8be9a6214
paramjeet-dhiman <paramjeet@guise.ai>
2022-01-03 17:50:30 +0530

    .

--- src/app/components/Products/AOSOverview.js
+++ src/app/components/Products/AOSOverview.js
@@ -75,7 +75,7 @@ export const AOSOverview = () => {
   ];
 
   return (
-    <div className="bg-white h-full ">
+    <div className="bg-white h-full select-none ">
       <section
         className="w-full bg-center bg-cover  "
         style={{
@@ -424,7 +424,7 @@ export const AOSOverview = () => {
           </div>
           <div
             className="lg:container mt-10 shadow-xl"
-            title="Click on the image or drag with bar to see results">
+            title="Slide the bar to see the difference that Guise AI can make">
             <ReactCompareImage
               leftImage={Airport1}
               rightImage={Airport2}
@@ -435,6 +435,24 @@ export const AOSOverview = () => {
               sliderLineColor="#8AEA92"
               sliderLineWidth={4}
             />
+          </div>
+          <div className="flex items-center justify-center space-x-1 font-light text-color-300 pt-4 ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+            <span>
+              Slide the bar to see the difference that Guise AI can make
+            </span>
           </div>
         </section>
 
