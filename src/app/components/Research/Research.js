d4622d13600c260e9c9a991cb1947f0f2a8e27e0
paramjeet-dhiman <paramjeet@guise.ai>
2022-01-18 11:13:05 +0530

    .

--- src/app/components/Research/Research.js
+++ src/app/components/Research/Research.js
@@ -1,35 +1,27 @@
 import React from "react";
+import { HeroHeader } from "../HeroHeader/HeroHeader";
 import { ResearchData } from "./ResearchData";
 
 export const Research = () => {
   return (
-    <div className="bg-white h-full select-none">
-      <section
-        className="w-full bg-center bg-cover  "
-        style={{
-          height: "36rem",
-          // boxShadow: `inset 0 0 0 2000px rgb(0 0 0 / 9%)`,
-          background: `url(https://images.pexels.com/photos/159775/library-la-trobe-study-students-159775.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940) center center / cover no-repeat`,
-        }}>
-        <div className="flex  items-center justify-center w-full h-full bg-black bg-opacity-80">
-          <div className="text-center px-10 lg:px-0">
-            <h1 className="text-4xl font-bold text-white uppercase md:text-7xl mb-4">
-              Research At Guise
-            </h1>
-          </div>
-        </div>
-      </section>
+    <div className="bg-white">
+      <HeroHeader
+        img={
+          "https://images.pexels.com/photos/159775/library-la-trobe-study-students-159775.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        }
+        title="Research"
+      />
 
       {ResearchData.map((item, index) => (
         <section
-          className=" bg-white py-10 lg:py-20 px-10 lg:px-0 container "
+          className=" bg-white py-10 lg:py-10 mt-10 px-10 lg:px-0 container w-full "
           key={index}>
           <a href={item.link} target="_blank" rel="noreferrer">
-            <h1 className="text-bookmark-blue underline text-xl lg:text-3xl hover:text-blue-500  tracking-wide leading-normal font-black mb-4">
+            <h1 className="text-xl lg:text-3xl  font-semibold  lg:leading-tight hover:underline   hover:text-blue-500 ">
               {item.title}
             </h1>
           </a>
-          <p className="text-lg lg:text-xl font-light leading-normal">
+          <p className=" mt-2 lg:mt-4 text-justify text-lg lg:text-xl">
             {item.description}
           </p>
         </section>
