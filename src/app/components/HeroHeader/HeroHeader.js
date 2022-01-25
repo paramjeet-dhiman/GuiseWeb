1bc4eb10f681ca23bee6865806a3b3e33c8069b9
paramjeet-dhiman <paramjeet@guise.ai>
2022-01-26 00:40:48 +0530

    .

--- src/app/components/HeroHeader/HeroHeader.js
+++ src/app/components/HeroHeader/HeroHeader.js
@@ -1,14 +1,22 @@
 import React from "react";
 
-export const HeroHeader = ({ img, title }) => {
+export const HeroHeader = ({ img, title, bgColor = "" }) => {
   return (
     <div>
       <section
         className="w-full bg-center bg-cover mt-10"
-        style={{
-          height: "36rem",
-          background: `url(${img}) center center / cover no-repeat`,
-        }}>
+        style={
+          img
+            ? {
+                height: "36rem",
+                background: `url(${img}) center center / cover no-repeat`,
+              }
+            : {
+                height: "36rem",
+
+                backgroundColor: "#0e141b",
+              }
+        }>
         <div className="flex items-center justify-center w-full h-full  bg-black bg-opacity-70">
           <div className="text-center  px-10 lg:px-0">
             <h1 className="text-4xl font-bold  text-white uppercase md:text-7xl filter drop-shadow-2xl">
--- src/app/pages/PrivacyPolicy.js
+++ src/app/pages/PrivacyPolicy.js
@@ -4,22 +4,21 @@ import { HeroHeader } from "../components/HeroHeader/HeroHeader";
 export const PrivacyPolicy = () => {
   return (
     <div className="">
-      <HeroHeader
-        img={
-          "https://images.unsplash.com/photo-1461685265823-f8d5d0b08b9b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-        }
-        title="Privacy Policy"
-      />
+      <HeroHeader title="Privacy Policy" />
       <section className="py-10 lg:py-20 px-10 lg:px-0 container ">
         <div className="space-y-5 lg:space-y-10 ">
           <div className="space-y-5 text-lg md:text-xl">
-            <h4 className="font-semibold  text-3xl">
-              At Guise AI we believe in privacy. Your data is your data, period.
-            </h4>
-            <h4 className="font-semibold text-2xl">
-              We do not collect your "personal data" when you visit our website.
-            </h4>
             <div className="space-y-5 text-xl ">
+              <p>
+                {" "}
+                At Guise AI we believe in privacy. Your data is your data,
+                period.
+              </p>
+              <p>
+                {" "}
+                We do not collect your "personal data" when you visit our
+                website.
+              </p>
               <p>
                 Why do we not collect your "personal data" when you browse our
                 website? We feel the best way to protect you and your data is to
